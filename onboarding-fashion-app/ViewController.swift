ddd2d8810daa3ea5d7c30555f75e5f45da77ea7d
htkien <htkien2511@gmail.com>
2020-03-31 17:43:32 +0700

    Initial Commit

---
+++ onboarding-fashion-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onboarding-fashion-app
+//
+//  Created by Hoang Trong Kien on 3/31/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
