01e152aa0e77b7378fc117c91f8708bd7ca5004e
htkien <htkien2511@gmail.com>
2020-04-01 18:12:59 +0700

    Initial Commit

--- onboarding-fashion-app/Views/QuoteCollectionViewCell.swift
+++ onboarding-fashion-app/Views/QuoteCollectionViewCell.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Hoang Trong Kien. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol QuoteCollectionViewCellDelegate: class {
+    func didTapExploreButton()
+}
+
+class QuoteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var exploreButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    weak var delegate: QuoteCollectionViewCellDelegate?
+    
+    func configure(with item: OnboardingItem) {
+        titleLabel.text = item.title
+        detailLabel.text = item.detail
+    }
+    
+    func showExploreButton(shouldShow: Bool) {
+        exploreButton.isHidden = !shouldShow
+    }
+    
+    @IBAction func exploreButtonTapped(_ sender: Any) {
+        delegate?.didTapExploreButton()
+    }
+}
