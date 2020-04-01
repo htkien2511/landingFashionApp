9af671937149dd2bd06b9748e55186976caa810e
htkien <htkien2511@gmail.com>
2020-04-01 18:07:23 +0700

    initial commit

---
+++ onboarding-fashion-app/Controllers/OnboardingViewController.swift
@@ -0,0 +1,172 @@
+//
+//  ViewController.swift
+//  onboarding-fashion-app
+//
+//  Created by Hoang Trong Kien on 3/31/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var imageViews = [UIImageView]()
+    
+    private var items: [OnboardingItem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPlaceholderItems()
+        setupCollectionView()
+        setupPageControl()
+        setupImageViews()
+    }
+    
+    private func setupPlaceholderItems() {
+        items = OnboardingItem.placeholderItems
+    }
+    
+    private func setupImageViews() {
+        items.forEach { item in
+            let imageView = UIImageView(image: item.image)
+            imageView.contentMode = .scaleAspectFill
+            imageView.alpha = 0.0
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.clipsToBounds = true
+            containerView.addSubview(imageView)
+            NSLayoutConstraint.activate([
+                imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.8),
+                imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+                imageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                imageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+            ])
+            imageViews.append(imageView)
+        }
+        imageViews.first?.alpha = 1.0
+        containerView.bringSubviewToFront(collectionView)
+    }
+    
+    private func setupPageControl() {
+        pageControl.numberOfPages = items.count
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        let nextRow = getCurrentIndex() + 1
+        let nextIndexPath = IndexPath(row: nextRow, section: 0)
+        collectionView.scrollToItem(at: nextIndexPath, at: .left, animated: true)
+        showItem(at: nextRow)
+    }
+    
+    private func showItem(at index: Int) {
+        pageControl.currentPage = index
+        let shouldHide = index == items.count - 1
+        nextButton.isHidden = shouldHide
+    }
+    
+    private func getCurrentIndex() -> Int {
+        return Int(collectionView.contentOffset.x / collectionView.frame.width)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let x = scrollView.contentOffset.x
+        let index = getCurrentIndex()
+        let fadeInAlpha = (x - collectionViewWidth * CGFloat(index)) / collectionViewWidth
+        let fadeOutAlpha = 1 - fadeInAlpha
+        guard index < items.count - 1 else {return}
+        imageViews[index].alpha = fadeOutAlpha
+        imageViews[index + 1].alpha = fadeInAlpha
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let index = getCurrentIndex()
+        showItem(at: index)
+    }
+    
+    var collectionViewWidth: CGFloat {
+        return collectionView.frame.size.width
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! QuoteCollectionViewCell
+        let shouldShow = indexPath.item == items.count - 1
+        cell.showExploreButton(shouldShow: shouldShow)
+        let item = items[indexPath.item]
+        cell.delegate = self
+        cell.configure(with: item)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension OnboardingViewController: QuoteCollectionViewCellDelegate {
+    func didTapExploreButton() {
+        
+        let mainAppViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "MainAppViewController")
+        if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate, let window = sceneDelegate.window {
+            window.rootViewController = mainAppViewController
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        }
+    }
+    
+    
+}
+
+protocol QuoteCollectionViewCellDelegate: class {
+    func didTapExploreButton()
+}
+
+class QuoteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var exploreButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    weak var delegate: QuoteCollectionViewCellDelegate?
+    
+    func configure(with item: OnboardingItem) {
+        titleLabel.text = item.title
+        detailLabel.text = item.detail
+    }
+    
+    func showExploreButton(shouldShow: Bool) {
+        exploreButton.isHidden = !shouldShow
+    }
+    
+    @IBAction func exploreButtonTapped(_ sender: Any) {
+        delegate?.didTapExploreButton()
+    }
+}
---
+++ onboarding-fashion-app/Models/OnboardingItem.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingItem.swift
+//  onboarding-fashion-app
+//
+//  Created by Hoang Trong Kien on 4/1/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import Foundation
